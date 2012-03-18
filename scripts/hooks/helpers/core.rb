4fe148aff0832f56e0f8e91abea11856caa7882e
Tony Arcieri <tony.arcieri@gmail.com>
2012-03-18 16:10:52 -0700

    Initial helpers and !calc

---
+++ scripts/hooks/helpers/core.rb
@@ -0,0 +1,24 @@
+module Helpers
+  module Core
+    # Usage info
+    def usage(string)
+      if ARGV[0] == 'help'
+        puts string
+        exit 1
+      end
+    end
+    
+    # Obtain all arguments as a single string
+    def input
+      ARGV[0..-1].join(' ') if ARGV[0]
+    end
+    
+    # Who was this script called by?
+    def called_by
+      ENV['CALLED_BY']
+    end
+    alias_method :who, :called_by
+  end
+end
+
+include Helpers::Core
